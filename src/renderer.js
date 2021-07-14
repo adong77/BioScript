bb325ef9bbe3a4e94153c4195aed49c41ee9dd8c
adong77 <youdng@hotmail.com>
2021-07-14 17:18:07 +0800

    create new window

--- src/renderer.js
+++ src/renderer.js
@@ -1,10 +1,22 @@
 
 const { remote: { BrowserWindow}, shell } = require('electron');
 
-// console.log("Hi:", __dirname);
-
+console.log("Hi:", __dirname);
+//Button
 const button = document.querySelector('.alert');
 
 button.addEventListener('click', () => {
     alert(__dirname);
 });
+
+
+// new window
+const newWindow = document.querySelector('.new-window')
+
+newWindow.onclick = function () {
+    bmWindow = new BrowserWindow({
+        width: 700,
+        height: 500
+    })
+    bmWindow.webContents.loadFile('../scripts/Reverse_complement_DNA_sequences.html');
+}
