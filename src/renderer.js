4dc62ea4d56a60b84f5d4b32ad95bbc0fa610c95
adong77 <youdng@hotmail.com>
2021-08-09 21:41:09 +0800

    afa0809

--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,7 @@
 const { remote: { BrowserWindow}, shell } = require('electron');
 
 console.log("Hi:", __dirname);
+
 //Button
 const button = document.querySelector('.alert');
 
@@ -18,7 +19,7 @@ newWindow.onclick = function () {
         width: 800,
         height: 600
     })
-    bmWindow.webContents.loadFile('../scripts/Reverse_complement_DNA_sequences.html');
+    bmWindow.webContents.loadFile('./demo.html');
 }
 
 //open explorer to open html
