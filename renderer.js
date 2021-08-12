c4a49c971e30b04cdbb01a15a5a11309838242c4
Adong77 <youdng@hotmail.com>
2021-08-12 21:59:02 +0800

    sadas

---
+++ renderer.js
@@ -0,0 +1,32 @@
+
+const { ipcRenderer, remote: { BrowserWindow}, shell } = require('electron');
+
+console.log("Hi:", __dirname);
+
+//Button
+const button = document.querySelector('.alert');
+
+button.addEventListener('click', () => {
+    alert(__dirname);
+});
+
+
+// new window
+const newWindow = document.querySelector('.new-window')
+
+newWindow.onclick = function () {
+    bmWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    bmWindow.webContents.loadFile(__dirname + 'src/demo.html');
+}
+
+//open explorer to open html
+const allA = document.querySelectorAll('a')
+allA.forEach(item => {
+    item.onclick = function(e){
+        e.preventDefault();
+        shell.openExternal(item.href)
+    }
+})
